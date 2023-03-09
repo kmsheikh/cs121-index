291a54b783e4520795aba32409baca5654e5fb83
Kiran Maya Sheikh <kmsheikh@circinus-27.ics.uci.edu>
2023-03-08 21:56:33 -0800

    Index offloaded in loop, partial indexes with duplicates

--- indexer.py
+++ indexer.py
@@ -32,8 +32,10 @@ import string
         # write to index.txt (complete index)
         # close a.txt
 
+ZIP_DOC_NUM = 2000                  # Number of documents in Zip File
 
 def indexer():
+
     # create partial-index directory
     try:
         os.mkdir("partial-index")
@@ -43,24 +45,23 @@ def indexer():
         return
 
     # OPEN ALL FILES IN partial-index directory
-    index_range = string.ascii_lowercase + string.digits + "*"
-    partial_dict = {}                                           # dictionary to track open index files
+    index_range = string.ascii_lowercase + string.digits + "!"
+    partial_dict = {}                                           # Dictionary to track open index files
 
     for i in index_range:
         path = "partial-index/" + i + ".txt"
         partial_file = open(path, "x", encoding="utf-8") 
-        partial_dict[i] = partial_file                          # match file to letter/digit
+        partial_dict[i] = partial_file                          # Match file to letter/digit
     
 
     # Iterate through the json files found in the zip file
     index = defaultdict(lambda: defaultdict(lambda: [0, 0]))
     lookup_file = open("docID.txt", "a", encoding="utf-8")
-    reject_file = open("reject.txt", "a", encoding="utf-8")     # CHECKING REJECTS
+    offload_dict = True
 
     with zipfile.ZipFile("analyst.zip", "r") as zipped:
         files = zipped.namelist()
         docID = 0
-        reject_count = 0        # CHECKING REJECTS
 
         for name in files:
             extension = os.path.splitext(name)[-1]  
@@ -71,40 +72,48 @@ def indexer():
                     json_dict = json.loads(json_content)
                     
                     page_soup = BeautifulSoup(json_dict['content'], "html.parser")
-                    check_html = page_soup.find_all("html")
                     
-                    if check_html:
-                        docID += 1
-                        lookup_file.write("{} {}\n".format(docID, json_dict["url"]))              # Append to docID lookup table
+                    text = page_soup.find_all(["title", "h1", "h2", "h3", "b", "strong"])       # Get "important" words from html
+                    important_text_size = len(text)     
+                    for chunk in text:
+                        word_list = tokenize_words(chunk.get_text())
+                        word_freq = computeWordFrequencies(word_list)       
+                        for key in word_freq:
+                            index[key][docID][1] += word_freq[key] 
 
-                        text = page_soup.find_all(["p", "pre", "li", "h4", "h5", "h6"])           # Get non-important words from html
-                
-                        for chunk in text:
-                            word_list = tokenize_words(chunk.get_text())
-                            word_freq = computeWordFrequencies(word_list)       
-                            for key in word_freq:
-                                index[key][docID][0] += word_freq[key] 
 
-                        text = page_soup.find_all(["title", "h1", "h2", "h3", "b", "strong"])     # Get "important" words from html
+                    unwanted_tags = ["title", "h1", "h2", "h3", "b", "strong"]
+                    for tag in unwanted_tags:
+                        [s.extract() for s in page_soup(tag)]
+
+                    text = page_soup.find_all()                                                 # Get "non-important" words from html
+                    text_size = len(text)
+                    for chunk in text:
+                        word_list = tokenize_words(chunk.get_text())
+                        word_freq = computeWordFrequencies(word_list)       
+                        for key in word_freq:
+                            index[key][docID][0] += word_freq[key]
+
+                    if text_size + important_text_size > 0:                                     # Check if indexer extracted text from tags
+                        docID += 1                                                              # If not, ignore
+                        lookup_file.write("{} {}\n".format(docID, json_dict["url"]))            # Append to docID lookup table
+
                     
-                        for chunk in text:
-                            word_list = tokenize_words(chunk.get_text())
-                            word_freq = computeWordFrequencies(word_list)  
-                            for key in word_freq:
-                                index[key][docID][1] += word_freq[key] 
-                    else:
-                        reject_count += 1
-                        reject_file.write("{} {}\n".format(reject_count, json_dict["url"]))     # CHECKING REJECTS
+                    # Offload the dictionary index in each of these branches
+                    if offload_dict == True:
+                        if docID > (ZIP_DOC_NUM / 5) * 4:
+                            index = offload(index, partial_dict)
+                            offload_dict = False
+                        elif docID > (ZIP_DOC_NUM / 5) * 3:
+                            index = offload(index, partial_dict) 
+                        elif docID > (ZIP_DOC_NUM / 5) * 2:
+                            index = offload(index, partial_dict)
+                        elif docID > ZIP_DOC_NUM / 5:
+                            index = offload(index, partial_dict)
 
-    lookup_file.close()
-    reject_file.close()     # CHECKING REJECTS
     
-
-
-    index = offload(index, partial_dict)
-    print ("Emptied index?")
-    print(index.items())
-
+    lookup_file.close()
+    offload(index, partial_dict)                    # Last offload
 
 
     #index_list = sorted(index.items(), key=lambda x: (x[0]))                    # Sort the index (ENTIRELY IN MEMORY)
@@ -123,13 +132,14 @@ def indexer():
      #       index_file.write("\n")
     
    # vocab_file.close()
-    
+
+
+
     # Close all open partial indexes
     for key in partial_dict.keys():
         partial_dict[key].close()
 
-    print("Done closing.")
-        
+
 
 
 # Separate function for offloading dict + writing to partial files
@@ -137,13 +147,12 @@ def indexer():
 
 def offload(my_dict, p_dict)->defaultdict:
     index_list = sorted(my_dict.items(), key=lambda x: (x[0]))          # Sort the current in-memory index
-                                                                        # list[-] = tuple(-, tuple(-, -))
-
+                                            
     for elem in index_list:
         try:
             partial_file = p_dict[elem[0][0]]                           # Use first letter to retrieve file   
         except KeyError:
-            partial_file = p_dict["*"]                                  # Non-english go in *.txt
+            partial_file = p_dict["!"]                                  # Non-english go in *.txt
 
         partial_file.write("{} ".format(elem[0])),
         
