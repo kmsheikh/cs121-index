502d63212edcfc83eee11ecb32d68523257990c9
Andres De La Torre <delatorreandres4@gmail.com>
2023-03-08 18:37:48 -0800

    Changed the html tags text is extracted from and added branches in which to offload current index.

--- indexer.py
+++ indexer.py
@@ -2,14 +2,10 @@ import os
 import json
 import zipfile
 from bs4 import BeautifulSoup
-import time
-import tokenizer
 import sys
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+from tokenizer import tokenize_words
 from tokenizer import computeWordFrequencies
 from collections import defaultdict
-import psutil
 import sys
 # create an inverted index for the corpus
 # tokens = alphanumeric sequences in the dataset
@@ -24,14 +20,14 @@ import sys
 # posting = representation of token's occurrence in document (doc id/name + tf-idf score)
 
 # m1 -- use token frequency instead of tf-idf score
-
+ZIP_DOC_NUM = 2000  # Get the amount of documents that are assumed to be in the Zip File
 
 def indexer():
     # Iterate through the json files found in the zip file
     index = defaultdict(lambda: defaultdict(lambda: [0, 0]))
     lookup_file = open("docID.txt", "a", encoding="utf-8")
     
-    with zipfile.ZipFile("developer.zip", "r") as zipped:
+    with zipfile.ZipFile("analyst.zip", "r") as zipped:
         files = zipped.namelist()
         docID = 0
 
@@ -46,16 +42,6 @@ def indexer():
                     page_soup = BeautifulSoup(json_dict['content'], "html.parser")   
                     lookup_file.write("{} {}\n".format(docID, json_dict["url"]))              # Append to docID lookup table
 
-                    print(f"{psutil.virtual_memory()[2]} percent of RAM used at DOC # {docID}. Size of DICT is {sys.getsizeof(index)}")
-                    
-                    text = page_soup.find_all(["p", "pre", "li", "h4", "h5", "h6"])           # Get non-important words from html
-                    text_size = len(text)       # Get size of prior find_all call
-                    for chunk in text:
-                        word_list = tokenize_words(chunk.get_text())
-                        word_freq = computeWordFrequencies(word_list)       
-                        for key in word_freq:
-                            index[key][docID][0] += word_freq[key] 
-
                     text = page_soup.find_all(["title", "h1", "h2", "h3", "b", "strong"])     # Get "important" words from html (replaces text for memory conservation)
                     important_text_size = len(text) # Get size of prior find_all call
                     for chunk in text:
@@ -63,9 +49,30 @@ def indexer():
                         word_freq = computeWordFrequencies(word_list)  
                         for key in word_freq:
                             index[key][docID][1] += word_freq[key] 
+
+                    unwanted_tags = ["title", "h1", "h2", "h3", "b", "strong"]
+                    for tag in unwanted_tags:
+                        [s.extract() for s in page_soup(tag)]
+
+                    text = page_soup.find_all()           # Get non-important words from html
+                    text_size = len(text)       # Get size of prior find_all call
+                    for chunk in text:
+                        word_list = tokenize_words(chunk.get_text())
+                        word_freq = computeWordFrequencies(word_list)       
+                        for key in word_freq:
+                            index[key][docID][0] += word_freq[key] 
                     
                     if text_size + important_text_size > 0: # Check if indexer extracted some text from the tags we require. If not it is essentially ignored.
                         docID += 1
+
+                    if docID > (ZIP_DOC_NUM / 5) * 4:  # Offload the dictionary index in each of these branches.
+                        pass
+                    elif docID > (ZIP_DOC_NUM / 5) * 3:
+                        pass 
+                    elif docID > (ZIP_DOC_NUM / 5) * 2:
+                        pass
+                    elif docID > ZIP_DOC_NUM / 5:
+                        pass
 
 
     lookup_file.close()
