c70d8d1bd13a300ae7233d62cd77d2c65f896d8a
Kiran Maya Sheikh <116920219+kmsheikh@users.noreply.github.com>
2023-03-14 12:57:53 -0700

    Adding submitted files into demo branch

---
+++ README.txt
@@ -0,0 +1,13 @@
+﻿indexer.py
+
+
+In order to run indexer.py, you must have a stopwords.txt file in the same directory. The stopwords.txt file contains all the stopwords, and is used for document processing. To get web pages from a specific zip file other than developer.zip, the zip file name that is opened at line 61 needs to be changed to the appropriate zip file name. If working with a different zip file 
+
+
+After running indexer.py, a folder with multiple partial indexes will be created. Additionally, a text file named docID.txt will be created, which is used for storing the urls of documents, and a text file named vocab.txt will be created, which is used for indexing the index.
+
+
+search.py
+
+
+In order to run search.py, you must have all the documents generated from indexer.py. After running search.py you will be prompted for a query. After entering your query, you will see the top 5 urls that match with your query. In order to stop the program, press ctrl+C.
--- search.py
+++ search.py
@@ -12,11 +12,10 @@ from tokenizer import *
 import glob
 from ntpath import basename
 import sys
-import asyncio
 import signal
 import time
 import glob
-from ntpath import basename
+import ntpath
 import sys
 import asyncio
 import signal
@@ -41,19 +40,69 @@ def search_engine():
             lookup_dict[int(ID)] = doc
     
     # INTRO TEXT
-    # explain what to search
-    # how to quit (after every timestamp?)
+    print("\tTo quit, enter ctrl+c.\n")
 
 
     # GET KEYBOARD INPUT
     while(1):
-        query = input("Enter your query:\n\t")
-        start = time.time()                                         # TIME SEARCH RESULTS       
-        postings_lists = gather_postings(query, vocab_dict, index_dict, stop_list)
+        query = input("Enter your query:\t")
+        start = time.time()                                         # TIME SEARCH RESULTS
+
+        postings_lists = gather_postings(query, vocab_dict, g_index_dict, stop_list)
+        if not postings_lists:                                      # empty list, no postings found
+            print("\n\tNo results found.")
+            print("\tTo quit, enter ctrl+c.\n")
+            continue
         
+        score_dict = boolean_retrieval(postings_lists)   
+        if not score_dict:
+            print("\n\tNo results found.")
+            print("\tTo quit, enter ctrl+c.\n")
+            continue
+    
+        ranked_list = sorted(score_dict.items(), key=lambda x: (x[1]), reverse=True)
+#print(ranked_list)
+
+        if len(ranked_list) < 10:
+            num = len(ranked_list)
+        else:
+            num = 10                                                # Sort and print top 10 or less results
+
+        for x in range(num):
+            doc = ranked_list[x][0]
+            print("\n\t{}\t{}\n".format(x+1, lookup_dict[doc]))
+
+        end = time.time()
+        time_elapsed = end - start
+
+        print("\n\tResults found in {} seconds.".format(time_elapsed))
+        print("\tTo quit, enter ctrl+c.\n")
 
 
-def gather_postings(the_query: str, vocab_dict: dict, index_dict: dict, stop_list: list)->list:
+
+def boolean_retrieval(postings_lists: list)->dict:
+    new_dict = {}
+    minimum_dict = min(postings_lists, key=len)                     # Use the smallest posting to compare
+    for key in minimum_dict.keys():
+        the_flag = True
+        add_score = 0
+
+        for posting in postings_lists:                              # If key exists in each dict, keep adding score
+            if key in posting.keys():
+                add_score += posting[key]
+            else: 
+                the_flag = False                                    # Else, stop looping through postings
+                break
+
+        if the_flag:                                                # TRUE = all postings had the document
+            new_dict[key] = add_score                               # Add the calculated score to the new dict (docID: score)
+      
+
+    return new_dict
+
+
+
+def gather_postings(the_query: str, vocab_dict: dict, g_index_dict: dict, stop_list: list)->list:
     query_list = tokenize_words(the_query)
     query_set = set()
     for word in query_list:
@@ -61,7 +110,7 @@ def gather_postings(the_query: str, vocab_dict: dict, index_dict: dict, stop_lis
             query_set.add(word)                                     # Lose duplicates and stop words in query
 
     # GET WORD and POSTINGS FROM THE INDEX
-    found_list = extract_postings(vocab_dict, query_list, index_dict)      
+    found_list = extract_postings(vocab_dict, query_list, g_index_dict)      
     if len(found_list) < len(query_set) or len(found_list) == 0:    # If not all search terms found, return empty list
         return []
 
@@ -70,24 +119,31 @@ def gather_postings(the_query: str, vocab_dict: dict, index_dict: dict, stop_lis
     for found in found_list:    
         entry_list = found.split()                                  # Split entry by whitespace: [word  1/0.0  2/0.0] 
         entry_list.pop(0)                                           # Remove token from parsed entry_list
-        postings_lists.append(entry_list)                                      # Append [1/0.0 2/0.0] to postings_list
+        
+        entry_dict = {}
+        for i in range(len(entry_list)):
+            entry_list[i] = entry_list[i].split("/")
+            entry_dict[int(entry_list[i][0])] = float(entry_list[i][1]) 
+
+        postings_lists.append(entry_dict)                           # Append [1: 0.0, 2: 0.0] to postings_list
 
     return postings_lists
 
 
-def extract_postings(vocab_dict: dict, query_set: set, index_dict: dict) -> list:
+
+def extract_postings(vocab_dict: dict, query_set: set, g_index_dict: dict) -> list:
     postings = []
     for word in query_set:
         if word in vocab_dict:
-            index_dict[word[0]].seek(vocab_dict[word])  
-            postings.append(index_dict[word[0]].readline())   # Append line found in partial_index
+            g_index_dict[word[0]].seek(vocab_dict[word])  
+            postings.append(g_index_dict[word[0]].readline())   # Append line found in partial_index
 
     return postings
 
 
 
-async def sigint_handler(g_index_dict, signum, frame):
-    print("\n\tExiting...\n")
+def sigint_handler(g_index_dict, signum, frame):
+    print("\n\n\tExiting...\n")
     
     # CLOSE ALL OPEN INDEXES
     for key in g_index_dict.keys():
@@ -98,13 +154,15 @@ async def sigint_handler(g_index_dict, signum, frame):
 
 
 if __name__ == "__main__":
+    print("\n\tStarting up search engine...")
+
     # OPEN INDEX FILES, save opened files in dict for closing later
     g_index_dict = {}
-    index_files = glob.glob("patial-index/*") 
+    index_files = glob.glob("partial-index/*") 
     for file in index_files:
-        txt = basename(file)                                                # Retrieve tail of path "a.txt" with basename
+        txt = ntpath.basename(file)                                         # Retrieve tail of path "a.txt" with basename
         g_index_dict[txt[0]] = open(file, "r", encoding="utf=8")
 
-    signal.signal(signal.SIGINT, partial(sigint_handler, g_index_dict)      # Install signal handler for crtl+C
+    signal.signal(signal.SIGINT, partial(sigint_handler, g_index_dict))     # Install signal handler for crtl+C
     
     search_engine()
